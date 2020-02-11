06168a0a6685a5e83349c6a88658c691c4886208
Marco Fanti <marco.fanti@gmail.com>
2020-02-11 08:36:09 -0500

    first commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+platform :ios, '9.0'
+
+target 'ChatExample' do
+  use_frameworks!
+  pod 'MessageKit', :path => '../'
+  pod 'InputBarAccessoryView'
+
+target 'ChatExampleTests' do
+  inherit! :search_paths
+end
+
+target 'ChatExampleUITests' do
+  inherit! :search_paths
+end
+
+end
---
+++ Sources/View Controllers/LaunchViewController.swift
@@ -0,0 +1,111 @@
+/*
+ MIT License
+ 
+ Copyright (c) 2017-2019 MessageKit
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import UIKit
+import MessageKit
+import SafariServices
+
+final internal class LaunchViewController: UITableViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    let cells = ["Banking Demo (DialogFlow)", "Banking Demo (Watson)", "Banking Demo (Lex)", "Password Reset (DialogFlow)", "Settings", "SSO Demo", "Company", "Solutions"]
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Demos"
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.tableFooterView = UIView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = true
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = false
+        }
+    }
+    
+    // MARK: - UITableViewDataSource
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cells.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+        cell.textLabel?.text = cells[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = cells[indexPath.row]
+        switch cell {
+        case "Banking Demo (DialogFlow)":
+            navigationController?.pushViewController(BasicExampleViewController(), animated: true)
+        case "Banking Demo (Watson)":
+            navigationController?.pushViewController(AdvancedExampleViewController(), animated: true)
+        case "Banking Demo (Lex)":
+            navigationController?.pushViewController(AutocompleteExampleViewController(), animated: true)
+        case "Password Reset (DialogFlow)":
+            navigationController?.pushViewController(MessageContainerController(), animated: true)
+        case "Settings":
+            navigationController?.pushViewController(SettingsViewController(), animated: true)
+        case "Company":
+            guard let url = URL(string: "https://www.behaviosec.com/") else { return }
+            openURL(url)
+        case "Solutions":
+            guard let url = URL(string: "https://www.behaviosec.com/behavioral-biometric-solutions/") else { return }
+            openURL(url)
+        case "SSO Demo":
+            guard let url = URL(string: "https://ping.behaviosec.com:9031/idp/startSSO.ping?PartnerSpId=https%3A%2F%2Fsaml.salesforce.com") else { return }
+            openURL(url)
+        default:
+            assertionFailure("You need to implement the action for this cell: \(cell)")
+            return
+        }
+    }
+    
+    func openURL(_ url: URL) {
+        let webViewController = SFSafariViewController(url: url)
+        if #available(iOS 10.0, *) {
+            webViewController.preferredControlTintColor = .primaryColor
+        }
+        present(webViewController, animated: true, completion: nil)
+    }
+}
